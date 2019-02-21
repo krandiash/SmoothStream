193331e16e3574dc275f8dc4835f00214b171e03
Karan Goel <kgoel93@gmail.com>
2019-02-21 13:30:25 -0800

    test

--- StreamViewer.py
+++ StreamViewer.py
@@ -196,7 +196,7 @@ def visualize(img, proc_param, joints, verts, cam, renderer):
 if __name__ == '__main__':
     sess, model, config = setup()
     print ("Setup is complete")
-    streamer = Streamer('DN51v4t5.SUNet', '8080')
+    streamer = Streamer('171.64.70.21', '8080')
     main(sess, model, config, streamer)
 
 
