b338d76fd6bc8d924002fcbfa2831d2c1e0ed276
Karan Goel <kgoel93@gmail.com>
2019-03-27 14:49:09 -0700

    changes

--- viewer.py
+++ viewer.py
@@ -61,7 +61,7 @@ class StreamViewer:
         self.keep_running = True
         frames_processed = 0
 
-        store_folder = "/next/u/kgoel/pose_estimation/data/%s" % (time.strftime("%b_%d_%Y_%H_%M_%S"))
+        store_folder = "/next/u/kgoel/pose_estimation/data/%s/" % (time.strftime("%b_%d_%Y_%H_%M_%S"))
         os.makedirs(store_folder, exist_ok=True)
 
         params = dict()
@@ -92,7 +92,7 @@ class StreamViewer:
 
                 print (dir(datum))
                 print(datum.poseKeypoints)
-                cv2.imwrite(store_folder + 'id', datum.cvOutputData)
+                cv2.imwrite(store_folder + 'id.jpg', datum.cvOutputData)
 
                 frames_processed += 1
                 print("FPS", frames_processed/ float(time.time() - start))
