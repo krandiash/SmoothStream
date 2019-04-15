89310fde00629e451dbb356ba7ce7978bbfea727
Karan Goel <kgoel93@gmail.com>
2019-04-15 13:52:48 -0700

    updated for real time

--- viewer.py
+++ viewer.py
@@ -29,7 +29,7 @@ class StreamViewer:
         """
         context = zmq.Context()
         self.footage_socket = context.socket(zmq.SUB)
-        self.footage_socket.bind('tcp://*:' + port)
+        self.footage_socket.bind('tcp://*:' + str(port))
         self.footage_socket.setsockopt_string(zmq.SUBSCRIBE, np.unicode(''))
         self.current_frame = None
         self.keep_running = True
